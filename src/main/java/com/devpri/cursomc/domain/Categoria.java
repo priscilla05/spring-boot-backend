18e655137c977d4b5ade92d3a66eb402b705d96a
PriscillaCorreia <primariafc@gmail.com>
2023-04-10 17:44:06 -0300

    Testando a primeira classe de dominio Categoria

--- src/main/java/com/devpri/cursomc/domain/Categoria.java
+++ src/main/java/com/devpri/cursomc/domain/Categoria.java
@@ -1,6 +1,10 @@
 package com.devpri.cursomc.domain;
 
-public class Categoria {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+    private static final long serialVersionUID =1L;
 
     private Integer id;
     private String nome;
@@ -27,6 +31,20 @@ public class Categoria {
     }
 
     public void setNome(String nome) {
+
         this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria)) return false;
+        Categoria categoria = (Categoria) o;
+        return id.equals(categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- src/main/java/com/devpri/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/devpri/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package com.devpri.cursomc.resources;
 
+import com.devpri.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "REST está funcionando";
+    public List<Categoria> listar(){
+        Categoria categoria1 = new Categoria(1, "Informática");
+        Categoria categoria2 = new Categoria(2, "Escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(categoria1);
+        lista.add(categoria2);
+
+        return lista;
     }
 }
