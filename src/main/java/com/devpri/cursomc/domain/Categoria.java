1f3369e0529d5ce364bdc88f90174f25e968d1a6
PriscillaCorreia <primariafc@gmail.com>
2023-04-12 13:05:53 -0300

    banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/devpri/cursomc/domain/Categoria.java
+++ src/main/java/com/devpri/cursomc/domain/Categoria.java
@@ -1,11 +1,18 @@
 package com.devpri.cursomc.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable {
     private static final long serialVersionUID =1L;
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
